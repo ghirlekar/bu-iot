5df3a9ed41cdae9dbdbe22eb21ed32f7f35d472a
Gaurav Hirlekar <gaurav.hirlekar@gmail.com>
2016-05-24 02:17:28 +0530

    Added more comments

--- application.cpp
+++ application.cpp
@@ -2,13 +2,14 @@
 #include "Adafruit_BMP085/Adafruit_BMP085.h"
 #include "MQTT/MQTT.h"
 
-#define CONNECTION_TIMEOUT_MSEC     4000
-#define TCP_FLUSH_DELAY_MSEC        1000
+#define CONNECTION_TIMEOUT_MSEC     4000    // Timeout interval for MQTT server
+#define TCP_FLUSH_DELAY_MSEC        1000    // Emperically found delay required to flush TCP segments before going to sleep.
 #define DEEP_SLEEP_PERIOD_SEC       5
-#define NUM_READINGS                3
+#define NUM_READINGS                3       // Device caches this many readings per sensor during a period equal to NUM_READINGS * DEEP_SLEEP_PERIOD_SEC before reporting.
 #define NUM_SENSORS                 4
 #define MQTT_SERVER_URI             "broker.hivemq.com"
 #define MQTT_SERVER_PORT            1883
+#define MQTT_CLIENT_NAME            "bu-sph-iot"
 #define PIN_PIR_SENSOR              A5
 #define PIN_CO_SENSOR               A1
 const String sensorTypes[] = { "temp", "pressure", "co", "motion" };
@@ -18,7 +19,7 @@ STARTUP(System.enableFeature(FEATURE_RETAINED_MEMORY));   // Enable Backup RAM f
 retained uint32_t timestamps[NUM_READINGS];
 retained float data[NUM_READINGS][NUM_SENSORS];
 retained uint16_t wakeCount;
-retained bool deviceInit = false;
+retained bool deviceInit = false;   // Flag to detect first startup
 
 void callback(char* topic, uint8_t* payload, unsigned int length) {};
 MQTT client(MQTT_SERVER_URI, MQTT_SERVER_PORT, callback);
@@ -31,7 +32,7 @@ void setup() {
         wakeCount = 0;
         Particle.connect();         // Connecting to Particle cloud syncs RTC
     }
-    pinMode(D7, OUTPUT);
+    pinMode(D7, OUTPUT);        // Status indicator LED on D7.
     pinMode(PIN_PIR_SENSOR, INPUT);
     pinMode(PIN_CO_SENSOR, INPUT);
     bmp.begin();
@@ -51,9 +52,11 @@ void setup() {
         digitalWrite(D7, !digitalRead(D7));
         delay(100);
       };
-      client.connect("bu-sph-iot");
+      client.connect(MQTT_CLIENT_NAME);
       uint32_t connectionTimeout = millis() + CONNECTION_TIMEOUT_MSEC;
       while(!client.isConnected()){
+        digitalWrite(D7, !digitalRead(D7));
+        delay(100);
         if(millis() > connectionTimeout)  return;
       }
       for(int i=0; i<NUM_SENSORS; i++){
@@ -69,7 +72,7 @@ void setup() {
       client.disconnect();
       delay(TCP_FLUSH_DELAY_MSEC);    // This delay allows time to flush TCP output buffer
     }
-    System.sleep(SLEEP_MODE_DEEP, DEEP_SLEEP_PERIOD_SEC);  // Switches WiFi and MCU off
+    System.sleep(SLEEP_MODE_DEEP, DEEP_SLEEP_PERIOD_SEC);  // Disconnects from Particle cloud and switches WiFi and MCU off
 }
 
 void loop(){}
